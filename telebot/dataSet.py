c1a0723d631d0ebba92cb3c87cc741fabca7dad7
kassem <omarksm09@gmail.com>
2022-10-02 16:07:40 +0200

    add bot files

---
+++ telebot/dataSet.py
@@ -0,0 +1,50 @@
+import csv
+import os
+from venv import create
+
+
+class Topics:
+    def createTopic():
+        cw = csv.writer(open("topics.csv", "w"))
+        data = [
+            "How to create a Python package in 2022",
+            "https://mathspp.com/blog/how-to-create-a-python-package-in-2022",
+        ]
+        with open("topics.csv", "w", encoding="UTF8") as f:
+            writer = csv.writer(f)
+            # write the data
+            writer.writerow(data)
+
+    def getTopic():
+        topics = []
+        if not os.path.exists("./topics.csv"):
+            Topics.createTopic()
+        with open("./topics.csv", "r") as file:
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                topics.append({"title": row[0], "link": row[1]})
+
+        return topics
+
+    def addTopic(title, link):
+        data = [title, link]
+        with open("topics.csv", "a", encoding="UTF8") as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
+
+
+class Users:
+    def addUser(id):
+        data = [id]
+        with open("users.csv", "a", encoding="UTF8") as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
+
+    def getUsers():
+        users = []
+        if os.path.exists("./users.csv"):
+            with open("./users.csv", "r") as file:
+                csvreader = csv.reader(file)
+                for row in csvreader:
+                    users.append(row[0])
+        return users
---
+++ telebot/listener.py
@@ -0,0 +1,78 @@
+import random
+import os
+
+
+from dataSet import Topics
+import telebot
+from dotenv import load_dotenv
+from telebot import types
+
+
+load_dotenv()
+bot = telebot.TeleBot(os.environ.get("API_KEY"), parse_mode=None)
+
+
+
+topics = Topics.getTopic()
+
+
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup.add(types.InlineKeyboardButton(text="Read awesome topic", callback_data="/get"))
+markup.add(types.InlineKeyboardButton(text="Add new topic", callback_data="/newTopic"))
+
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(
+        message,
+        "Hello, how are you doing?\nDo you want to read awesome topic?",
+        reply_markup=markup,
+    )
+
+
+@bot.message_handler(commands=["newTopic", "help"])
+def startNewTopic(message):
+    sent_msg = bot.send_message(message.chat.id, "enter the title of the topic")
+    bot.register_next_step_handler(sent_msg, newTopic)
+
+
+# @bot.message_handler(commands=["get"])
+@bot.message_handler(func=lambda message: True)
+def sendTopic(message):
+    if message.text == "Read awesome topic":
+        try:
+            topic = random.choice(topics)
+        except:
+            topic = topics[0]
+
+        title = topic["title"]
+        link = topic["link"]
+        bot.send_message(message.chat.id, f" {title} {link}")
+    elif message.text == "Add new topic":
+        sent_msg = bot.send_message(message.chat.id, "enter the title of the topic")
+        bot.register_next_step_handler(sent_msg, newTopic)
+    else:
+        bot.reply_to(message, "invalid message", reply_markup=markup)
+
+
+def newTopic(message):
+    def getLink(message):
+        link = message.text
+        if message.text == "cancel" or message.text == "Cancel":
+            bot.reply_to(message, "canceled")
+            return
+        Topics.addTopic(title, link)
+        bot.reply_to(message, "topic added")
+
+    if message.text == "cancel" or message.text == "Cancel":
+        bot.reply_to(message, "canceled")
+        return
+
+    title = message.text
+    sent_msg = bot.send_message(
+        message.chat.id, f"Topic title is {title}\n please enter the link of the topic"
+    )
+    bot.register_next_step_handler(sent_msg, getLink)
+
+
+bot.polling()
